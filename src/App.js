cfa8fdf0a6501c459815251abb13917154a67331
Josh Stevens <joshstevens19@hotmail.co.uk>
2020-09-04 16:16:03 +0100

    handle the restore authenticated task complete event

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 import logo from './logo.svg';
-import { isAuthenticated$ } from './services/store';
+import {
+  isAuthenticated$,
+  restoreAuthenticationTaskCompleted$,
+} from './services/store';
 import { login, logout } from './services/wallet-service';
 import WalletFollower from './Wallet-Follower';
 
 class App extends Component {
   constructor(props) {
     super();
-    this.state = { isLoggedIn: false };
+    this.state = { isLoggedIn: false, loading: true };
     isAuthenticated$.subscribe((value) => {
       this.setState({ isLoggedIn: value });
       this.forceUpdate();
+    });
+
+    restoreAuthenticationTaskCompleted$.subscribe((value) => {
+      if (value) {
+        this.setState({ isLoggedIn: isAuthenticated$.value, loading: false });
+        this.forceUpdate();
+      }
     });
   }
 
@@ -20,10 +30,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          {!this.state.isLoggedIn ? (
+          {this.state.loading ? <p>Loading please wait</p> : null}
+          {!this.state.isLoggedIn && !this.state.loading ? (
             <button onClick={login}>Login</button>
           ) : null}
-          {this.state.isLoggedIn ? (
+          {this.state.isLoggedIn && !this.state.loading ? (
             <button onClick={logout}>Logout</button>
           ) : null}
           {this.state.isLoggedIn ? <WalletFollower /> : null}
