02fdb516230b6d61ecb6e3a755592818476721ba
Josh Stevens <joshstevens19@hotmail.co.uk>
2020-09-04 16:08:56 +0100

    add no border to leader

--- src/Wallet-Leader.js
+++ src/Wallet-Leader.js
@@ -14,6 +14,7 @@ class WalletLeader extends Component {
               is-fun-wallet="true"
               is-leader="true"
               onLoad={this.yourLeaderInstanceLoadFunction}
+              frameBorder="0"
             ></iframe>
           </div>
         </div>
