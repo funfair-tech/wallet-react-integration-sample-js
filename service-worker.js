ff166b6a61efb772b78a693a6436e8362783ecaf
Josh Stevens <joshstevens19@hotmail.co.uk>
2020-09-08 09:36:55 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://github.com/funfair-tech/wallet-react-integration-sample-js /precache-manifest.95b053e79713253c411660a45744843a.js"
+  "https://github.com/funfair-tech/wallet-react-integration-sample-js/precache-manifest.c4983b021261bb590d31f19d2b02b2ed.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://github.com/funfair-tech/wallet-react-integration-sample-js /index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://github.com/funfair-tech/wallet-react-integration-sample-js/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
