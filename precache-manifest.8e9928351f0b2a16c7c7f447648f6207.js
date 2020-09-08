45c89251dff8e7e9b9ae1d55fb3eec5111407485
Josh Stevens <joshstevens19@hotmail.co.uk>
2020-09-08 10:12:59 +0100

    Updates

---
+++ precache-manifest.8e9928351f0b2a16c7c7f447648f6207.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e81c7830ba6bcd5d60a306ef2f081d0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14d843d51cee6709530b",
+    "url": "/static/css/main.fab01b89.chunk.css"
+  },
+  {
+    "revision": "78de148f0cf34637c01e",
+    "url": "/static/js/2.517a5be8.chunk.js"
+  },
+  {
+    "revision": "c28e5e8434dfd32c13e7e7d25169bf37",
+    "url": "/static/js/2.517a5be8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14d843d51cee6709530b",
+    "url": "/static/js/main.f475e62f.chunk.js"
+  },
+  {
+    "revision": "9b3671aa4382dea0d050",
+    "url": "/static/js/runtime-main.f6028ede.js"
+  },
+  {
+    "revision": "02850c4cabcbbe69b13b1281d108b5dd",
+    "url": "/static/media/logo.02850c4c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/funfair-tech/wallet-react-integration-sample-js/precache-manifest.dcd2c4aac76f8837419e11a5df85f70f.js"
+  "/precache-manifest.8e9928351f0b2a16c7c7f447648f6207.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/funfair-tech/wallet-react-integration-sample-js/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
